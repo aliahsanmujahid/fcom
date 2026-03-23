168ae2fd2c84289322149c949a0cecfaac0110bb
aliahsanmujahid <ahsanalimujahid@gmail.com>
2026-03-24 01:17:26 +0600

    ff

--- c/src/environments/environment.ts
+++ c/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend.bdcloth.com/api/',
-  adminsite: 'https://backend.bdcloth.com/',
-  // apiUrl: 'http://127.0.0.1:8000/api/',
-  // adminsite: 'http://127.0.0.1:8000/',
+  // apiUrl: 'https://backend.bdcloth.com/api/',
+  // adminsite: 'https://backend.bdcloth.com/',
+  apiUrl: 'http://127.0.0.1:8000/api/',
+  adminsite: 'http://127.0.0.1:8000/',
   skip: 10,
   oskip: 5,
 };
--- d/src/environments/environment.ts
+++ d/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://backend.bdcloth.com/api/',
-  // apiUrl: 'http://localhost:8000/api/',
+  // apiUrl: 'https://backend.bdcloth.com/api/',
+  apiUrl: 'http://localhost:8000/api/',
   skip: 10,
   oskip: 5,
 };
