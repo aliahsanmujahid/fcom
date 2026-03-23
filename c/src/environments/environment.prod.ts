04d0b74c48b1393bf18393f28d5d1873a2a2879f
aliahsanmujahid <ahsanalimujahid@gmail.com>
2026-03-24 01:37:49 +0600

    xx

--- c/src/environments/environment.prod.ts
+++ c/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   // apiUrl: 'https://backend.bdcloth.com/api/',
   // adminsite: 'https://backend.bdcloth.com/',
-  apiUrl: 'http://127.0.0.1:8000/api/',
-  adminsite: 'http://127.0.0.1:8000/',
+  apiUrl: 'http://localhost:8000/api/',
+  adminsite: 'http://localhost:4201/',
   skip: 10,
   oskip: 5,
 };
--- d/src/environments/environment.prod.ts
+++ d/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://backend.bdcloth.com/api/',
-  apiUrl: 'http://127.0.0.1:8000/api/',
+  apiUrl: 'http://localhost:8000/api/',
   skip: 10,
   oskip: 5,
 };
+
+
